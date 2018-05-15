e9233961df4bd6ca428be5cb9db427e6608f2fe3
Fela Maslen <felamaslen@gmail.com>
2018-05-15 20:22:17 +0100

    Finished drawing walls

--- src/Pacman/constants.js
+++ src/Pacman/constants.js
@@ -3,7 +3,7 @@ export const DIRECTION_NORTH = 1;
 export const DIRECTION_WEST = 2;
 export const DIRECTION_SOUTH = 3;
 
-export const BOARD_WIDTH = 29;
+export const BOARD_WIDTH = 28;
 export const BOARD_HEIGHT = 36;
 
 export const WALL_COLOUR = '#06f';
